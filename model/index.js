3f0636c44dca5f198a954bb92bb68277016591d4
Vlad <vladhopilo@gmail.com>
2021-07-01 00:36:09 +0300

    getContact

--- model/index.js
+++ model/index.js
@@ -1,7 +1,19 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
 // const contacts = require('./contacts.json')
+const path = require("path")
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+ try {
+   const file = await fs.readFile(contactsPath)
+        const data = JSON.parse(file)
+        // console.table(data)
+        return data
+ } catch (error) {
+   error.message = "listContacts error";
+        throw new Error(error.message)
+    }
+}
 
 const getContactById = async (contactId) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const {  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact} = require("../../model")
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    res.json({
+        status: "success",
+        code: 200,
+        data: {
+            result: await listContacts(),
+        }
+    })
 })
 
 router.get('/:contactId', async (req, res, next) => {
