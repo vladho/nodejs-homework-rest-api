9be62ca80fcb30db03d26cff81adae3cf03cc1b3
Vlad <vladhopilo@gmail.com>
2021-07-07 15:57:24 +0300

    done

--- controllers/auth/currentUser.js
+++ controllers/auth/currentUser.js
@@ -10,8 +10,8 @@ const currentUser = async (req, res, next) => {
       });
     }
     const currentUser = {
-      email: req.user.email,
-      subscription: req.user.subscription,
+      email: user.email,
+      subscription: user.subscription,
     };
 
     res.status(200).json({
